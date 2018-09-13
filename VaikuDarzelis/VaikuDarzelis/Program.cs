c1f982f5ea7244cd423fec042a2179463ff13900
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2018-09-13 13:18:40 +0300

    create object for lines

--- VaikuDarzelis/VaikuDarzelis/Program.cs
+++ VaikuDarzelis/VaikuDarzelis/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,32 @@ namespace VaikuDarzelis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+
+            args = new[] { @"C:\Users\Paulius\Documents\GitHub\VaikuDarzelis\DarzelioVaikai.csv" };
+
+            var transactions = ReadTransaction(args[0]);
+
+            /*try
+            {
+                var text = File.ReadAllText(@"C:\Users\Paulius\Documents\GitHub\VaikuDarzelis\DarzelioVaikai.csv");
+                Console.WriteLine(text);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+            
+        }
+
+        static IList<Transaction> ReadTransaction(string path, bool hasHeader = true ) {
+
+            var list = new List<Transaction>();
+            foreach(var line in File.ReadLines(path).Skip(hasHeader ? 1 : 0))
+            {
+                list.Add(Transaction.Fromline());
+            }
+            return list;
         }
     }
-}
+} 
+       
---
+++ VaikuDarzelis/VaikuDarzelis/Transaction.cs
@@ -0,0 +1,23 @@
+﻿namespace VaikuDarzelis
+{
+    public class Transaction
+    {
+
+        public int DARZ_ID { get; set; }
+        public string SCHOOL_NAME { get; set; }
+        public int TYPE_ID { get; set; }
+        public string TYPE_LABEL { get; set; }
+        public int LAN_ID { get; set; }
+        public string LAN_LABEL { get; set; }
+        public int CHILDS_COUNT { get; set; }
+        public int FREE_SPACE { get; set; }
+        public static Transaction FromLine(string line)
+        {
+            var data = line.Split(',');
+            return new Transaction
+            {
+
+            };
+        }
+    }
+}
